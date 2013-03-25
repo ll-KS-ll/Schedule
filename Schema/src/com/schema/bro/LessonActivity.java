779fa91b7943f6c1a89f9fe493cab51a95de8c81
Filip <fbrolund@gmail.com>
2013-03-25 22:37:31 +0100

    Changed the layout of lesson

--- Schema/src/com/schema/bro/LessonActivity.java
+++ Schema/src/com/schema/bro/LessonActivity.java
@@ -13,9 +13,12 @@ import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TimePicker;
@@ -31,26 +34,46 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 	private EditText edit_name;
 	@SuppressWarnings("unused")
 	private static final String TAG = "DialogActivity";
-	private static final int DIALOG1 = 0, DIALOG2 = 1, DIALOG3 = 2;
+	private static final int DIALOG1 = 0, DIALOG2 = 1;
 	private static final int TEXT_ID = 0;
 	private boolean isStartTime = false;
+	private ImageView image;
+	private int val = 1;
 
-	String room, teacher, startTime, endTime, otherInfo;
+	int[] imageIDs = { R.drawable.pic1, 
+			R.drawable.pic2, 
+			R.drawable.pic3,
+			R.drawable.pic4,
+			R.drawable.pic5,
+			R.drawable.pic6,
+			R.drawable.pic7,
+			R.drawable.pic8,
+			R.drawable.pic9,
+			R.drawable.pic10,
+			R.drawable.pic11,
+			R.drawable.pic12,
+			R.drawable.pic13,
+			R.drawable.pic14,
+			R.drawable.pic15,
+			R.drawable.pic16,
+			R.drawable.pic17,
+			};
+
+	String room, teacher, startTime, endTime;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lesson_activity);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		startTime = endTime = "08:20";
+		startTime = endTime = "00:00";
 		room = teacher = "";
 
 		cursor = new MatrixCursor(matrix);
 		cursor.addRow(new Object[] { 0, "Startar:", startTime });
 		cursor.addRow(new Object[] { 1, "Slutar:", endTime });
 		cursor.addRow(new Object[] { 2, "Sal:", room });
-		cursor.addRow(new Object[] { 3, "L√§rare:", teacher });
-		cursor.addRow(new Object[] { 4, "√ñvrigt:", otherInfo });
+		cursor.addRow(new Object[] { 3, "L‰rare:", teacher });
 
 		data = new SimpleCursorAdapter(this, R.layout.two_item_list_item,
 				cursor, columns, layouts, 0);
@@ -58,7 +81,34 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 		list = (ListView) findViewById(R.id.listView1);
 		list.setAdapter(data);
 		list.setOnItemClickListener(onListClick);
-		edit_name = (EditText) findViewById(R.id.editText1);
+		list.setScrollContainer(false);
+		edit_name = (EditText) findViewById(R.id.lessonText);
+
+		image = (ImageView) findViewById(R.id.lessonImage);
+		image.setImageResource(imageIDs[0]);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.done, menu);
+
+		return true;
+	}
+
+	public void onClick(final View v) {
+		switch (v.getId()) {
+		case R.id.prevImageButton:
+			val--;
+			if (val < 0)
+				val = imageIDs.length - 1;
+			break;
+		case R.id.nextImageButton:
+			val++;
+			if (val > imageIDs.length - 1)
+				val = 0;
+			break;
+		}
+		image.setImageResource(imageIDs[val]);
 	}
 
 	public void UpdateListView() {
@@ -70,8 +120,7 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 		cursor.addRow(new Object[] { 0, "Startar:", startTime });
 		cursor.addRow(new Object[] { 1, "Slutar:", endTime });
 		cursor.addRow(new Object[] { 2, "Sal:", room });
-		cursor.addRow(new Object[] { 3, "L√§rare:", teacher });
-		cursor.addRow(new Object[] { 4, "√ñvrigt:", otherInfo });
+		cursor.addRow(new Object[] { 3, "L‰rare:", teacher });
 
 		data.changeCursor(cursor);
 
@@ -85,21 +134,24 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 		UpdateListView();
 	}
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.button1:
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.done:
 			String name = edit_name.getText().toString();
-			String data = Lesson.convertToString("M√•ndag", startTime, endTime, name, room, teacher);
-			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String data = Lesson.convertToString("MÂndag", startTime, endTime,
+					name, room, teacher);
+			SharedPreferences prefs = PreferenceManager
+					.getDefaultSharedPreferences(this);
 			int n = prefs.getInt("count", 0) + 1;
 			prefs.edit().putString("lesson_" + n, data).commit();
 			prefs.edit().putInt("count", n).commit();
 			finish();
 			break;
-		case R.id.button2:
-			finish();
-			break;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
+		return true;
 	}
 
 	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
@@ -124,9 +176,6 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 			case 3:
 				showDialog(DIALOG2);
 				break;
-			case 4:
-				showDialog(DIALOG3);
-				break;
 			}
 		}
 	};
@@ -141,9 +190,7 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 		case DIALOG1:
 			return createExampleDialog("Ange salens namn:", DIALOG1);
 		case DIALOG2:
-			return createExampleDialog("Ange l√§rarens namn:", DIALOG2);
-		case DIALOG3:
-			return createExampleDialog("√ñvrig information:", DIALOG3);
+			return createExampleDialog("Ange l‰rarens namn:", DIALOG2);
 		default:
 			return null;
 		}
@@ -184,10 +231,8 @@ public class LessonActivity extends Activity implements OnTimeSetListener {
 					public void onClick(DialogInterface dialog, int whichButton) {
 						if (id == DIALOG1)
 							room = input.getText().toString();
-						if (id == DIALOG2)
+						else if (id == DIALOG2)
 							teacher = input.getText().toString();
-						if (id == DIALOG3)
-							otherInfo = input.getText().toString();
 						UpdateListView();
 						return;
 					}
