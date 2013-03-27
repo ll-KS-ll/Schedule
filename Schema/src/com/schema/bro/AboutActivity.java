1de024ba0bfcc32d502562e5df93647417957de8
K-S <karlssonsehlin@gmail.com>
2013-03-27 19:47:14 +0100

    Added content in about page.

--- Schema/src/com/schema/bro/AboutActivity.java
+++ Schema/src/com/schema/bro/AboutActivity.java
@@ -1,21 +1,25 @@
 package com.schema.bro;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
+public class AboutActivity extends PreferenceActivity {
 
-public class AboutActivity extends Activity{
-
+	@SuppressWarnings("deprecation")
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		SharedPreferences mPrefs = getSharedPreferences("THEME", 0);
 		int themeID = mPrefs.getInt("theme_int", 0);
 		super.setTheme(themeID);
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about_activity);
 		
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+		addPreferencesFromResource(R.xml.about);
 		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 }
